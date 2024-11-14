3590a7473cafaebbd5a821783641a9af0870347a
2419746 <2419746@dundee.ac.uk>
2024-11-14 12:01:08 +0000

    connecting database to html

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+$host = "database-1.cyvzwzewn9v9.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "password123";
+$database = "database-1";
+$mysql = new PDO("mysql:host=".$host.";dbname=".$database,
+$username, $password);
+echo "Database Linked <br>";
+?> 
