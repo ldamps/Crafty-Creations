3f6c62ffbef4089fcc82375ef3cd9d3b89083f54
Abz1717 <2457570@dundee.ac.uk>
2024-11-18 08:54:21 +0000

    removing dedundant html code in php

--- index.php
+++ index.php
@@ -1,49 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crafty Creations</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <nav class="topnav">
-        <a class="company">Crafty Creations</a>
-        <ul>
-            <a href="loginPage.php">Log In</a>
-        </ul>
-    </nav>
+<?php
+require 'db.php'; 
 
-    <nav class="selection">
-        <input type="text" placeholder="Search..">
+include 'index.html';
 
-        <div class="yarn">
-            <button class="dropbtn">Yarn
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="yarn-selection">
-                <a href="#">Show All</a>
-                <a href="#">Acrylic Yarn</a>
-            </div>
-        </div>
-
-        <div class="fabric">
-            <button class="dropbtn">Fabric
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="fabric-selection">
-                <a href="#">Show All</a>
-            </div>
-        </div>
-
-        <div class="paint">
-            <button class="dropbtn">Paint
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="paint-selection">
-                <a href="#">Show All</a>
-            </div>
-        </div>
-    </nav>
-</body>
-</html>
+?>
