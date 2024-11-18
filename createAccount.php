2028373e7bd4b6fab0fbbd0303902156ea3232c6
Marylou7 <2501402@dundee.ac.uk>
2024-11-18 14:48:16 +0000

    adding new accounts

--- createAccount.php
+++ createAccount.php
@@ -50,38 +50,87 @@
     </style>
 </head>
 <body>
-    <center><h1>Create an account </h1></center>
-    <form>
+
+<?php
+session_start(); // start the session
+
+include 'db.php'; //database connection
+
+//if a form is submitted
+if (isset($_POST['submit'])) {
+
+        // prepare the SQL statement
+        $stmt = $mysql->prepare("INSERT INTO Customer (Title, FirstName, LastName, PhoneNumber, EmailAddress, Password)
+        VALUES (:Title, :FirstName, :LastName, :PhoneNumber, :EmailAddress, :Password)");
+        
+
+        // bind parameters
+        $stmt->bindParam(":Title", $title);
+        $stmt->bindParam(":FirstName", $firstname);
+        $stmt->bindParam(":LastName", $lastname);
+        $stmt->bindParam(":PhoneNumber", $phonenumber);
+        $stmt->bindParam(":EmailAddress", $emailaddress);
+        $stmt->bindParam(":Password", $password);
+
+        // assign variables
+        $title = $_POST['title'];
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $phonenumber = $_POST['phonenumber'];
+        $emailaddress = $_POST['emailaddress'];
+        $password = $_POST['password']; 
+
+        // execute the statement
+        $stmt->execute();
+
+         // store the success message 
+        $_SESSION['message'] = "Account created successfully";
+
+        // prevent resubmission
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit(); 
+}
+?>
+ <center><h1>Create an Account</h1></center>
+ 
+ <?php
+// check if the message is stored in the session
+if (isset($_SESSION['message'])) {
+    echo "<p style='color: green;'>" . $_SESSION['message'] . "</p>";
+    unset($_SESSION['message']); // clears the message
+}
+?>
+    <form name="Add New Customer Account" method="post" action="">
         <div class="createContainer">
             <label for="title">Title: </label>
-            <select name="titles" id="title">
-                <option value="miss">Miss</option>
-                <option value="mrs">Mrs</option>
-                <option value="ms">Ms</option>
-                <option value="dr">Dr</option>
-                <option value="mr">Mr</option>
-            </select>
+            <select name="title" id="title" required>
+                <option value="Miss">Miss</option>
+                <option value="Mrs">Mrs</option>
+                <option value="Ms">Ms</option>
+                <option value="Dr">Dr</option>
+                <option value="Mr">Mr</option>
+            </select><br>
 
-            <label>First name: </label>
-            <input type="text" placeholder="First name..." required>
+            <label for="firstname">First Name: </label>
+            <input type="text" id="firstname" name="firstname" placeholder="First name..." required><br>
 
-            <label>Last name: </label>
-            <input type="text" placeholder="Last name..." required>
+            <label for="lastname">Last Name: </label>
+            <input type="text" id="lastname" name="lastname" placeholder="Last name..." required><br>
 
-            <label>Email: </label>
-            <input type="text" placeholder="Email address..." required>
+            <label for="emailaddress">Email: </label>
+            <input type="text" id="emailaddress" name="emailaddress" placeholder="Email address..." required><br>
 
-            <label>Phone Number: </label>
-            <input type="text" placeholder="Phone number..." required>
+            <label for="phonenumber">Phone Number: </label>
+            <input type="text" id="phonenumber" name="phonenumber" placeholder="Phone number..." required><br>
 
-            <label>Password: </label>
-            <input type="password" placeholder="Choose password..." required>
+            <label for="password">Password: </label>
+            <input type="password" id="password" name="password" placeholder="Choose password..." required><br>
 
-            <label>Repeat password: </label>
-            <input type="password" placeholder="Repeat password..." required>
+            <label for="repeatpassword">Repeat Password: </label>
+            <input type="password" id="repeatpassword" name="repeatpassword" placeholder="Repeat password..." required><br>
 
-            <button type="submit">Create</button>
-
+            <button type="submit" name="submit" value="submit">Create</button>
+            
         </div>
     </form>
 </body>
