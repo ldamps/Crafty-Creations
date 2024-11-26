62c1f7431a535c7b7f3ad023821de894acee3d8d
FosEd <Fosterej@btinternet.com>
2024-11-26 20:15:54 +0000

    Possibly a fix for adding more than once?

--- addNewSupplier.php
+++ addNewSupplier.php
@@ -5,9 +5,9 @@ require 'db.php';
 include 'addNewSupplier.html';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "<p>post</p>";
+    // echo "<p>post</p>";
     if(isset($_POST['addSupplier'])){
-        echo "<h1>Supplie</h1>";
+        // echo "<h1>Supplie</h1>";
         $_SESSION['addSupplier'] = $_POST['addSupplier'];
     }
     if(isset($_SESSION['addSupplier'])){
@@ -17,20 +17,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 if (isset($_SESSION['addSupplier'])){
-    echo "<h1>Supplier Details</h1>";
+    // echo "<h1>Supplier Details</h1>";
     $name = $newSupplier[0];
     $address = $newSupplier[3];
     $email = $newSupplier[2];
     $supplyType = $newSupplier[1];
-    echo $name." ".$address." ".$email." ".$supplyType;
+    // echo $name." ".$address." ".$email." ".$supplyType;
 
     $query = "INSERT INTO Supplier (Name, Address, ProductTypeSupplied, Email) VALUES ('$name', '$address', '$supplyType', '$email')";
     $mysql->exec($query);
-    echo "<h1>Supplier Added</h1>";
-    echo "<a class = 'return' href = 'suppliers.php'><h4>Click to return to suppliers page</h4></a>";
+    // echo "<h1>Supplier Added</h1>";
+    // echo "<a class = 'return' href = 'suppliers.php'><h4>Click to return to suppliers page</h4></a>";
 
-    // unset($_SESSION['addSupplier']);
-    //unset($_POST['addSupplier']);
+    unset($_SESSION['addSupplier']);
+    unset($_POST['addSupplier']);
 }
 ?>
 
