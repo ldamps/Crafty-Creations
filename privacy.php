1da3a37b3b07323060244329d7e963f503f9c7e6
Lirit Dampier <2560877@dundee.ac.uk>
2024-11-28 11:07:47 +0000

    t&c+privacy policy edits

--- privacy.php
+++ privacy.php
@@ -1,3 +1,5 @@
+<?php include('navBar.php'); ?>
+ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,114 +8,83 @@
     <title>Crafty Creations</title>
     <link rel="stylesheet" href="style.css">
     <style>
+        body {
+            padding: 5px;
+        }
+ 
+        body a {
+            text-decoration: underline;
+        }
+ 
+ 
         footer {
-            background-color: #E2F8FB;
             box-sizing: border-box;
             overflow: hidden;
             padding-left: 0.5%;
             padding-right: 0.5%;
+            bottom:0;
         }
-
+ 
         h2 {
             text-decoration: underline;
             font-size: 15;
         }
-
-        body h1, h2, h3, h4, h5, p {
-            margin-left: 10px;
-        }
-
+ 
         footer p {
             font-size: 12;
             color: black;
         }
-
+ 
         footer a {
             font-size: 12;
         }
-
+ 
         footer a:hover {
             color: gray;
         }
-
+ 
         footer ul {
             font-size: 12;
         }
-
+ 
         .column {
             float: left;
             padding: 10px;
         }
-
+ 
         .About {
             width: 40%;
         }
-
+ 
         .Links {
             width: 20%;
         }
-
+ 
         .Contact {
             width: 35%;
         }
-
+ 
         .footerRow:after {
             content: "";
             display: table;
             clear: both;
         }
-
+ 
+       
+ 
         @media screen and (max-width: 600px) {
             .column {
             width: 100%;
             }
         }
-
+ 
         .copyright {
             background-color: darkgray;
         }
     </style>
 </head>
-
 <body>
-    <nav class="topnav">
-        <a class="company" href="index.php">Crafty Creations</a>
-        <ul>
-            <a id='logInText' class="button" href="loginPage.php">log in | sign up</a>
-        </ul>
-    </nav>
-
-    <nav class="selection">
-        <input type="text" placeholder="Search..">
-
-        <div class="yarn button">
-            <button class="dropbtn">Yarn
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="yarn-selection">
-                <a href="#">Show All</a>
-                <a href="#">Acrylic Yarn</a>
-            </div>
-        </div>
-
-        <div class="fabric button">
-            <button class="dropbtn">Fabric
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="fabric-selection">
-                <a href="#">Show All</a>
-            </div>
-        </div>
-
-        <div class="paint button">
-            <button class="dropbtn">Paint
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="paint-selection">
-                <a href="#">Show All</a>
-            </div>
-        </div>
-    </nav>
+    <br>
     <br>
     <h3>Privacy Policy for Crafty Creations</h3>
     <h4>Effective Date: 11/2024</h4>
@@ -178,34 +149,5 @@
     <p>This privacy policy should be tailored further to fit your specific business model, especially if you have any special practices, such as handling sensitive customer data or specific marketing techniques. Additionally, you may want to consult a legal professional to ensure it fully complies with applicable privacy laws depending on where your customers are located.</p>
     <br>
 </body>
-
-<footer>
-    <div class="footerRow">
-        <div class="column About">
-            <h2>About Us</h2>
-            <p>Crafty Creations is a leading crafts chain retailer which sells a variety of craft supplies and tools, including yarn, paint, fabric and other crafting essentials. With a growing number of physical stores within the UK and an online store, Crafty Creations has become the go-to stop for both beginner and professional crafters. With its wide range of products, the company allows artists to craft their dreams into reality!</p>
-        </div>
-        <div class="column Links">
-            <h2>Quick Links</h2>
-            <li><a href="loginPage.php">Log In</a></li>
-            <li><a href="about.html">About Us</a></li>
-            <li><a href="#">Tools</a></li>
-            <li><a href="#">Paint</a></li>
-            <li><a href="#">Fabric</a></li>
-            <li><a href="#">Yarn</a></li>
-            <li><a href="termsConditions.html">Terms and Conditions</a></li>
-        </div>
-        <div class="column Contact">
-            <h2>Contact Us</h2>
-            <ul>craftyEnquires@craftycreations.com</ul>
-            <br>
-            <ul>Alternatively if you would rather phone us, you can call us on the the following telephone number: +44 07856 987654</ul>
-        </div>
-    </div>
-
-    <div class="copyright">
-        <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by Crafty Creations</p>
-    </div>
-</footer>
-
-</html>
+ 
+<?php include('footer.html'); ?>
